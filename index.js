d8f3bb3d113606a842821a3a220797a2d18257a9
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-01 20:40:16 -0500

    created the user input questions for the shapes choices, color choices, text choices, and prompted inquirer async/await with them to wait on user input

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 
-const fs = require("fs")
-let SVG = require("./lib/svg")
+const inquirer = require('inquirer');
+const SVG = require("./lib/svg");
+const fs = require('fs');
+const { Circle, Triangle, Square } = require('./shapes');
 
 
-let example = new SVG("DAN", "green", "square", "yellow")
+const questions = [
+
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['Circle', 'Triangle', 'Square'],
+    },
+
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Type in a color, or hexadecimal number for color, that you want the shape to be:',
+      validate: (input) => {
+        // any custom validation rules for shape color input
+        return true;
+        }
+    },
+
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter UP TO three characters to display inside of the shape:',
+      validate: (input) => {
+        if (input.length > 3) {
+          return 'Your input must be 3 characters or less!';
+        }
+        // this arrow function says that the value of an input with 3 characters or less will return true,
+        // if the input is MORE that 3 characters, it will return false and then they will get the return message.
+        return true;
+        },
+    },
+
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Type in a color, or hexadecimal number for color, that you want the text inside to be:',
+      validate: (input) => {
+        // Add any custom validation rules for text color input
+        return true;
+      },
+    }];
 
 
 
-fs.writeFile("shape.svg", example.markUp, function(err){
-    if(err)
-    console.log(err)
-})
+//SVG file
+fs.writeFile("logo.svg", example.markUp,(err) => {
+    if (err) throw err;
+    console.log('Generated logo.svg');
+});
